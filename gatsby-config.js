ecdf01da6113dfd081746c585a323ad073d20647
Davide Imola <davide.imola@icloud.com>
2020-12-23 21:46:53 +0100

    Fix netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,13 +71,7 @@ module.exports = {
         icon: `content/assets/gatsby-icon.png`,
       },
     },
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: "gatsby-plugin-netlify-cache",
-      options: {
-        cachePublic: true
-      }
-    }
+    `gatsby-plugin-react-helmet`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
