e0582d4f31eaffd1cc85e2c739d0a46dc36d6258
Davide Imola <davide.imola@icloud.com>
2021-10-01 12:07:06 +0200

    Improve teaching and mentoring part of the site

--- src/pages/about-me.tsx
+++ src/pages/about-me.tsx
@@ -35,21 +35,53 @@ const AboutMe = ({data, location}) => {
           <h3>Aside from software engineering?</h3>
           In my spare time, I enjoy watching movies and series, listen to music, read some books (maybe I would read more than now 😅), play video games, stay with Lord (my dog, a Cavalier King Charles Spaniel 🐶) and travel around the world. Moreover, I am a newbie amatorial photographer.
 
-          <h3>Mentoring</h3>
-          I offer a 1-to-1 mentoring on different topics:
+          <h3>Mentoring and Teaching</h3>
+          In my spare time, I usually tend to help others as I can. So I have decided to start two different initiatives.
+
+          If you are eager to learn something on the following topics, please reach me with the options you think fit most of your needs.
 
           <ul>
+            <li>Start your career in Computer Science</li>
+            <li>Backend Development (JavaScript, TypeScript, Go, JAVA, and Python)</li>
             <li>DevOps</li>
             <li>Software Engineering</li>
             <li>Microservices Architectures</li>
-            <li>Kubernetes</li>
+            <li>Docker and Kubernetes</li>
             <li>Open Source</li>
             <li>Cyber Security</li>
           </ul>
 
+          <h4>Mentoring</h4>
+          I offer free 1-to-1 mentorship time to everyone interested have a guide. I can help you by giving you some advice to continue your journey in Computer Science.
+
+          In this time we can do different activities:
+
+          <ul>
+            <li>Talking about Computer Science</li>
+            <li>Talking how to became a better developer</li>
+            <li>Talking about DevOps stuff</li>
+            <li>Asking for advice</li>
+            <li>Coding review</li>
+            <li>Pair programming</li>
+            <li>Study something together</li>
+          </ul>
+
           Schedule a session by checking my availability on <a href="https://calendly.com/davideimola/30min" target="_blank">Calendly</a>!
 
-          Instead, if you are looking to learn things in more depth you can buy some of my hours on <a href="https://www.docety.com/?referral_code=COADWEZBKJPVTCC21" target="_blank">DOCETY</a>.
+          <h4>Teaching</h4>
+          Instead, if you are looking to learn things in more depth and you require some of my hours, you can buy them from <a href="https://www.docety.com/?referral_code=COADWEZBKJPVTCC21" target="_blank">DOCETY</a>.
+
+          In this time we can do different activities:
+
+          <ul>
+            <li>Programming lessons</li>
+            <li>IT Consultant activities</li>
+            <li>Teach you the base of Test Driven Deployment</li>
+            <li>Teach you the base of Git and GitHub</li>
+            <li>Teach you things about DevOps and Cloud Native</li>
+          </ul>
+
+          In case of a lesson, I will contact you before. I want to understand what you want to learn and prepare myself to be ready for you.
 
           <h3>Wanna help me and my projects?</h3>
           I spend time working and helping other people for <b>mutual improvement</b>! So any help are really appreciated.
