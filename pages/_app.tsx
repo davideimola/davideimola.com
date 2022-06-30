153fff2c59c8b60b9eef753a145cd9a404c797d8
Davide Imola <davide.imola@icloud.com>
2022-06-30 12:36:35 +0200

    feature: add cookies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,22 @@
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import {SEO} from "../components/seo";
+import Script from "next/script";
 
 function MyApp({Component, pageProps}: AppProps) {
     return (
         <>
             <SEO/>
             <Component {...pageProps} />
+
+            <Script id="iub" type="text/javascript">
+                {`
+var _iub = _iub || [];
+_iub.csConfiguration = {"consentOnContinuedBrowsing":false,"countryDetection":true,"enableCcpa":true,"invalidateConsentWithoutLog":true,"lang":"en","perPurposeConsent":true,"siteId":2109199,"whitelabel":false,"cookiePolicyId":20195792, "banner":{ "acceptButtonCaptionColor":"#FFFFFF","acceptButtonColor":"#0073CE","acceptButtonDisplay":true,"backgroundColor":"#FFFFFF","brandBackgroundColor":"#FFFFFF","brandTextColor":"#000000","customizeButtonCaptionColor":"#4D4D4D","customizeButtonColor":"#DADADA","customizeButtonDisplay":true,"explicitWithdrawal":true,"listPurposes":true,"position":"float-bottom-center","rejectButtonCaptionColor":"#FFFFFF","rejectButtonColor":"#0073CE","rejectButtonDisplay":true,"textColor":"#000000" }};
+`}
+            </Script>
+            <Script type="text/javascript" src="//cdn.iubenda.com/cs/ccpa/stub.js" />
+            <Script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charSet="UTF-8" async />
         </>
     );
 }
