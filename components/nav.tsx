1afedc9b62289f1eaf25429a8613f1d90b52a6ef
Davide Imola <davide.imola@icloud.com>
2022-08-11 15:24:19 +0200

    Test

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,10 +1,11 @@
-import { Disclosure } from "@headlessui/react";
+import { Disclosure, Popover, Transition } from "@headlessui/react";
 import { MenuIcon, SpeakerphoneIcon, XIcon } from "@heroicons/react/outline";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { navigation } from "../utils/nav";
 import Image from "next/image";
 import {config} from "../utils/config";
+import {Fragment, SVGProps} from "react";
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -17,74 +18,103 @@ export const Nav = () => {
     return router.pathname === path;
   };
 
-  return (
-    <div>
-      <Disclosure as="nav" className="bg-white shadow">
-        {({ open }) => (
-          <>
-            <div className="max-w-7xl mx-auto pt-6 px-4 sm:px-6 lg:px-8">
-              <div className="relative flex justify-between h-16">
-                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                  {/* Mobile menu button */}
-                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                    <span className="sr-only">Open main menu</span>
-                    {open ? (
-                      <XIcon className="block h-6 w-6" aria-hidden="true" />
-                    ) : (
-                      <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                    )}
-                  </Disclosure.Button>
-                </div>
-                <div className="relative flex items-center justify-between sm:h-10 lg:justify-start">
-                  <div className="flex-shrink-0 flex items-center text-indigo-600">
-                    <Link href="/">
-                      <a>
-                        <img
-                            alt="Davide Imola"
-                            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                            className="h-8 w-auto sm:h-10"
-                        />
-                      </a>
-                    </Link>
-                  </div>
-                  <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-                    {navigation.map((nav) => (
-                      <Link href={nav.href} key={nav.href}>
-                        <a
-                          className="font-medium text-gray-500 hover:text-gray-900"
-                        >
-                          {nav.name}
-                        </a>
-                      </Link>
-                    ))}
-                  </div>
-                </div>
-                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                </div>
-              </div>
-            </div>
-
-            <Disclosure.Panel className="sm:hidden">
-              <div className="pt-2 pb-4 space-y-1">
-                {navigation.map((nav) => (
-                  <Disclosure.Button as={Link} href={nav.href} key={nav.href}>
-                    <a
-                      className={classNames(
-                        "block pl-3 pr-4 py-2 border-l-4 text-base font-medium",
-                        linkActive(nav.href)
-                          ? "bg-indigo-50 border-indigo-500 text-indigo-700"
-                          : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700"
-                      )}
+ return (
+        <div className="relative bg-white overflow-hidden">
+            <div className="max-w-7xl mx-auto">
+                <div
+                    className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+                    <svg
+                        className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+                        fill="currentColor"
+                        viewBox="0 0 100 100"
+                        preserveAspectRatio="none"
+                        aria-hidden="true"
                     >
-                      {nav.name}
-                    </a>
-                  </Disclosure.Button>
-                ))}
-              </div>
-            </Disclosure.Panel>
-          </>
-        )}
-      </Disclosure>
-    </div>
-  );
+                        <polygon points="50,0 100,0 50,100 0,100"/>
+                    </svg>
+
+                    <Popover>
+                        <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+                            <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start"
+                                 aria-label="Global">
+                                <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                                    <div className="flex items-center justify-between w-full md:w-auto">
+                                        <Link href="/">
+                                            <span className="sr-only">Davide Imola</span>
+                                            <img
+                                                alt="Davide Imola"
+                                                className="h-8 w-auto sm:h-10"
+                                                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                                            />
+                                        </Link>
+                                        <div className="-mr-2 flex items-center md:hidden">
+                                            <Popover.Button
+                                                className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                                <span className="sr-only">Open main menu</span>
+                                                <MenuIcon className="h-6 w-6" aria-hidden="true"/>
+                                            </Popover.Button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+                                    {navigation.map((item) => (
+                                        <a key={item.name} href={item.href}
+                                           className="font-medium text-gray-500 hover:text-gray-900">
+                                            {item.name}
+                                        </a>
+                                    ))}
+                                </div>
+                            </nav>
+                        </div>
+
+                        <Transition
+                            as={Fragment}
+                            enter="duration-150 ease-out"
+                            enterFrom="opacity-0 scale-95"
+                            enterTo="opacity-100 scale-100"
+                            leave="duration-100 ease-in"
+                            leaveFrom="opacity-100 scale-100"
+                            leaveTo="opacity-0 scale-95"
+                        >
+                            <Popover.Panel
+                                focus
+                                className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+                            >
+                                <div
+                                    className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+                                    <div className="px-5 pt-4 flex items-center justify-between">
+                                        <div>
+                                            <img
+                                                className="h-8 w-auto"
+                                                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                                                alt=""
+                                            />
+                                        </div>
+                                        <div className="-mr-2">
+                                            <Popover.Button
+                                                className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                                <span className="sr-only">Close main menu</span>
+                                                <XIcon className="h-6 w-6" aria-hidden="true"/>
+                                            </Popover.Button>
+                                        </div>
+                                    </div>
+                                    <div className="px-2 pt-2 pb-3 space-y-1">
+                                        {navigation.map((item) => (
+                                            <a
+                                                key={item.name}
+                                                href={item.href}
+                                                className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                                            >
+                                                {item.name}
+                                            </a>
+                                        ))}
+                                    </div>
+                                </div>
+                            </Popover.Panel>
+                        </Transition>
+                    </Popover>
+                </div>
+            </div>
+        </div>
+    );
 };
